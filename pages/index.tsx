a43c6fa93e5c91751fac43418dcba16e85a4bf52
Héctor Fornes <hector.fornes@nunsys.com>
2020-06-24 14:42:18 +0200

    added Ant Design following with-antd-design-less example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
 import { Fragment } from 'react';
 import Head from 'next/head';
+import {
+  Form,
+  Select,
+  InputNumber,
+  DatePicker,
+  Switch,
+  Slider,
+  Button,
+} from 'antd';
 
 import useRequest from '../libs/useRequest';
+
+const FormItem = Form.Item;
+const Option = Select.Option;
 
 export const Home = (): JSX.Element => {
   const { data, error } = useRequest<string>({
@@ -30,6 +42,80 @@ export const Home = (): JSX.Element => {
             <p className="description">
               Get started by editing <code>pages/index.tsx</code>
             </p>
+
+            <div style={{ width: '100%' }}>
+              <Form layout="horizontal">
+                <FormItem
+                  label="Input Number"
+                  labelCol={{ span: 8 }}
+                  wrapperCol={{ span: 8 }}
+                >
+                  <InputNumber
+                    size="large"
+                    min={1}
+                    max={10}
+                    style={{ width: 100 }}
+                    defaultValue={3}
+                    name="inputNumber"
+                  />
+                  <a href="#">Link</a>
+                </FormItem>
+
+                <FormItem
+                  label="Switch"
+                  labelCol={{ span: 8 }}
+                  wrapperCol={{ span: 8 }}
+                >
+                  <Switch defaultChecked />
+                </FormItem>
+
+                <FormItem
+                  label="Slider"
+                  labelCol={{ span: 8 }}
+                  wrapperCol={{ span: 8 }}
+                >
+                  <Slider defaultValue={70} />
+                </FormItem>
+
+                <FormItem
+                  label="Select"
+                  labelCol={{ span: 8 }}
+                  wrapperCol={{ span: 8 }}
+                >
+                  <Select
+                    size="large"
+                    defaultValue="lucy"
+                    style={{ width: 192 }}
+                  >
+                    <Option value="jack">jack</Option>
+                    <Option value="lucy">lucy</Option>
+                    <Option value="disabled" disabled>
+                      disabled
+                    </Option>
+                    <Option value="yiminghe">yiminghe</Option>
+                  </Select>
+                </FormItem>
+
+                <FormItem
+                  label="DatePicker"
+                  labelCol={{ span: 8 }}
+                  wrapperCol={{ span: 8 }}
+                >
+                  <DatePicker name="startDate" />
+                </FormItem>
+                <FormItem
+                  style={{ marginTop: 48 }}
+                  wrapperCol={{ span: 8, offset: 8 }}
+                >
+                  <Button size="large" type="primary" htmlType="submit">
+                    OK
+                  </Button>
+                  <Button size="large" style={{ marginLeft: 8 }}>
+                    Cancel
+                  </Button>
+                </FormItem>
+              </Form>
+            </div>
           </Fragment>
         )}
       </main>
