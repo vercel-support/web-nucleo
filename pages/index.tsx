92f1c9b66c14593788adf30177660bf2816d3af2
Francisco Rubin Capalbo <frubin@mercadona.es>
2020-06-27 12:26:36 +0200

    cleaned up code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import { Fragment } from 'react';
 import { WithTranslation } from 'next-i18next';
 import Head from 'next/head';
 import { GetStaticProps } from 'next';
@@ -6,7 +5,6 @@ import { GetStaticProps } from 'next';
 import nextI18Next from '../i18n';
 import { salesforceClient } from '../backend/salesforce';
 import Flat from '../backend/salesforce/flat';
-import useRequest from '../libs/useRequest';
 
 const { withTranslation } = nextI18Next;
 
@@ -17,9 +15,6 @@ interface StaticProps {
 type Props = StaticProps & WithTranslation;
 
 export const Home = ({ flats, t }: Props): JSX.Element => {
-  const { data, error } = useRequest<string>({
-    url: '/api/hello',
-  });
   const flats_list = Flat.deserialize_results(flats);
 
   return (
@@ -30,131 +25,20 @@ export const Home = ({ flats, t }: Props): JSX.Element => {
       </Head>
 
       <main>
-        {error ? (
-          <div>Failed to load</div>
-        ) : !data ? (
-          <div>Loading...</div>
-        ) : (
-          <Fragment>
-            <h1 className="title">
-              Hello <span>{data}</span>, welcome to{' '}
-              <a href="https://nextjs.org">Next.js!</a>
-            </h1>
-
-            <p className="description">
-              Get started by editing <code>pages/index.tsx</code>
-            </p>
-            <ul>
-              {flats_list.map((flat) => {
-                return <li key={flat.name}>{flat.name}</li>;
-              })}
-            </ul>
-          </Fragment>
-        )}
+        <h1 className="title">Web Núcleo</h1>
+        <ul>
+          {flats_list.map((flat) => {
+            return (
+              <li key={flat.name}>
+                {flat.name}
+                {flat.picture_urls.map((url) => {
+                  return <img key={url} src={url}></img>;
+                })}
+              </li>
+            );
+          })}
+        </ul>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-        .title,
-        .description {
-          text-align: center;
-        }
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-        .logo {
-          height: 1em;
-        }
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 };
