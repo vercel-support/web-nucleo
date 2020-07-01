2ecc53c9471881730fe2dd993aee593bd543f01e
Francisco Rubin Capalbo <frubin@mercadona.es>
2020-07-01 12:37:11 +0200

    added integration with styled-components + theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,36 @@ import React from 'react';
 import App from 'next/app';
 import nextI18Next from '../i18n';
 
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+import theme from '../common/theme';
+
+import { CSSProp } from 'styled-components';
+
+declare module 'react' {
+  interface HTMLAttributes<T> extends DOMAttributes<T> {
+    css?: CSSProp;
+  }
+}
+
+const GlobalStyle = createGlobalStyle`
+  html, body
+  {
+      margin: 0px; 
+      padding: 0px;
+  }
+`;
+
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
 
-    return <Component {...pageProps} />;
+    return (
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    );
   }
 }
 
