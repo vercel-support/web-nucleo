21bebe0dfcfed565584e3d06fd8136bcd9236a9e
Francisco Rubin Capalbo <frubin@mercadona.es>
2020-07-04 18:30:43 +0200

    fix ssr for flatsDisplay

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,14 @@ import nextI18Next from '../i18n';
 
 import Flat from '../backend/salesforce/flat';
 import {
-  FlatsDisplay,
   BlogShowcase,
   Hero,
   NewsletterSection,
 } from '../components/home';
 import { Header, Footer } from '../components/shared';
+import dynamic from 'next/dynamic'
+
+const FlatsDisplay = dynamic(() => import('../components/home/flatsDisplay'))
 
 const { withTranslation } = nextI18Next;
 
