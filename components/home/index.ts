708286e02ba3fb7b22b83201d1a982c5a878ea21
Francisco Rubin Capalbo <frubin@mercadona.es>
2020-07-05 16:28:47 +0200

    fix linting

--- components/home/index.ts
+++ components/home/index.ts
@@ -1,4 +1,7 @@
 export { default as BlogShowcase } from './blogShowcase';
-export { FlatsDisplayPlaceholder, default as FlatsDisplay } from './flatsDisplay';
+export {
+  FlatsDisplayPlaceholder,
+  default as FlatsDisplay,
+} from './flatsDisplay';
 export { default as Hero } from './hero';
 export { default as NewsletterSection } from './newsletterSection';
