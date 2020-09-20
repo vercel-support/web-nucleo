0ec0183dd453b23f3f69e9b89ecd59feb2e64eb4
Héctor Fornes <hector.fornes@nunsys.com>
2020-09-20 22:54:44 +0200

    fixed xml error

--- pages/generate-sitemap.tsx
+++ pages/generate-sitemap.tsx
@@ -13,34 +13,29 @@ const generateSitemap = async (flatsIds: string[]) => {
     '!pages/sitemap.xml.tsx',
     '!pages/pisos/**',
   ]);
-  console.log(pages);
 
-  return `
-<?xml version="1.0" encoding="UTF-8"?>
+  return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${pages
-      .map((page) => {
-        const path = page.replace('pages', '').replace('.tsx', '');
-        const route = path === '/index' ? '' : path;
+  ${pages
+    .map((page) => {
+      const path = page.replace('pages', '').replace('.tsx', '');
+      const route = path === '/index' ? '' : path;
 
-        return `
-  <url>
+      return `<url>
     <loc>${`${SITE_URL}${route}`}</loc>
   </url>
-           `;
-      })
-      .join('')}
-    ${flatsIds
-      .map((flatId) => {
-        return `
-  <url>
+  `;
+    })
+    .join('')}
+  ${flatsIds
+    .map((flatId) => {
+      return `<url>
     <loc>${`${SITE_URL}/pisos/${flatId}`}</loc>
   </url>
-        `;
-      })
-      .join('')}
-</urlset>
   `;
+    })
+    .join('')}
+</urlset>`;
 };
 
 const GenerateSitemap: NextPage = () => {
