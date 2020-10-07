7a4e8633104abf6e07265d5572133428a9cf5ecc
Héctor Fornes <hector.fornes@nunsys.com>
2020-10-08 01:35:31 +0200

    changed sitemap generation

--- pages/generate-sitemap.tsx
+++ pages/generate-sitemap.tsx
@@ -2,15 +2,20 @@ import { NextPage, GetStaticProps } from 'next';
 import globby from 'globby';
 
 import Flat from '../backend/salesforce/flat';
+import { computeSearchOptions } from '../common/hooks/searchService';
 
 const SITE_URL = 'https://www.inmobiliarianucleo.com';
 
-const generateSitemap = async (flatsIds: string[]) => {
+const generateSitemap = async (
+  flatsIds: string[],
+  searchOptionsText: string[]
+) => {
   const pages = await globby([
     'pages/**/*.tsx',
     '!pages/_app.tsx',
     '!pages/_document.tsx',
     '!pages/generate-sitemap.tsx',
+    '!pages/buscar/**',
     '!pages/pisos/**',
   ]);
 
@@ -35,6 +40,14 @@ const generateSitemap = async (flatsIds: string[]) => {
   `;
     })
     .join('')}
+  ${searchOptionsText
+    .map((searchOptionText) => {
+      return `<url>
+    <loc>${`${SITE_URL}/buscar?q=${searchOptionText}`}</loc>
+  </url>
+  `;
+    })
+    .join('')}
 </urlset>`;
 };
 
@@ -44,7 +57,11 @@ const GenerateSitemap: NextPage = () => {
 
 export const getStaticProps: GetStaticProps = async () => {
   const flats = await Flat.getFlats();
-  const sitemap = await generateSitemap(flats.map((flat) => flat.id));
+  const searchOptions = computeSearchOptions(flats);
+  const sitemap = await generateSitemap(
+    flats.map((flat) => flat.id),
+    searchOptions.map((option) => option.text)
+  );
 
   const fs = require('fs');
   fs.writeFileSync('public/sitemap.xml', sitemap);
