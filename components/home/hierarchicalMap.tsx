bbcd34fe22ab48332edbb23c2fdf6f9d16e381cc
Francisco Rubin Capalbo <frubin@mercadona.es>
2020-12-21 21:23:50 +0100

    added svg prefetching on map

--- components/home/hierarchicalMap.tsx
+++ components/home/hierarchicalMap.tsx
@@ -219,6 +219,7 @@ const HierarchicalMap = ({ zones, className, theme }: Props): JSX.Element => {
     _setCurrentMapId('0');
   }
 
+  const zoneUrls = Object.values(zones).filter(zone => zone.url).map(zone => zone.url);
   return (
     <div className={className}>
       <Title>{i18n.t('home.map.title')}</Title>
@@ -263,6 +264,7 @@ const HierarchicalMap = ({ zones, className, theme }: Props): JSX.Element => {
           <Placeholder />
         )}
       </div>
+      {zoneUrls.map((zoneUrl) => (<link key={zoneUrl} rel="prefetch" href={zoneUrl} />))}
     </div>
   );
 };
